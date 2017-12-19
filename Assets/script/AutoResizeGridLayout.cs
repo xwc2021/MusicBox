2746e1746c55970814992237e5fb5dd16decbfaf
xiangwei71 <xiangwei71@gmail.com>
2017-12-19 16:41:12 +0800

    加入元件：AutoResizeGridLayout
    用以自動調整ScrollView、content的height。當加入新的子項目進入content時會用到。

---
+++ Assets/script/AutoResizeGridLayout.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(GridLayoutGroup))]
+public class AutoResizeGridLayout : MonoBehaviour {
+
+
+    [SerializeField]
+    private int itemCount=2;
+
+    private RectTransform targetRect;
+    private GridLayoutGroup gridLayoutGroup;
+
+    // Use this for initialization
+    void Awake () {
+        targetRect=this.GetComponent<RectTransform>();
+        gridLayoutGroup = this.GetComponent<GridLayoutGroup>();
+        setItemCount(itemCount);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //根據count更新RectTransform的height
+    public void setItemCount(int count) {
+        this.itemCount = count;
+
+        float rowCount = (count + 1) / 2;
+        float newHeight = rowCount * gridLayoutGroup.cellSize.y
+            + (rowCount + 1) * gridLayoutGroup.spacing.y;
+
+        //sizeDelta
+        //有Stretch時 正值代表加長，負值代表減短，0代表不變
+        //沒有Stretch時，則是設定width或height的值
+        
+        //下面有Stretch
+        targetRect.sizeDelta = new Vector2(0, newHeight);
+    }
+}
