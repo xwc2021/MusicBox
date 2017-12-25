554400573e29c634c5fb0e4a7ed6f0ee93de4127
xiangwei71 <xiangwei71@gmail.com>
2017-12-25 20:10:23 +0800

    sql schema

---
+++ musicbox_schema.sql
@@ -0,0 +1,66 @@
+CREATE TABLE lists (
+   id serial primary key,
+   listname character varying(50) NOT NULL,
+   description character varying(100) NOT NULL,
+   ispublic boolean NOT NULL
+);
+
+CREATE TABLE musics (
+   id serial primary key,
+   musicname character varying(50) NOT NULL,
+   description character varying(100) NOT NULL,
+   ispublic boolean NOT NULL,
+   movable boolean NOT NULL,
+   canref boolean NOT NULL
+);
+
+CREATE TABLE users (
+   userid character varying(100) primary key,
+   pwd character varying(100) NOT NULL
+);
+
+CREATE TABLE replys (
+   id serial primary key,
+   musicid integer references musics(id),
+   replycontent character varying(200) NOT NULL,
+   whoreply character varying(100) NOT NULL
+);
+
+CREATE TABLE subreplys (
+   id serial primary key,
+   replyid integer references replys(id),
+   replycontent character varying(100) NOT NULL,
+   whoreply character varying(100) NOT NULL,
+   replyto character varying(100) NOT NULL
+);
+
+--多對多關聯
+
+CREATE TABLE userlist (
+   id serial primary key,
+   userid character varying(100) references users(userid),
+   listid integer references lists(id),
+   isref boolean DEFAULT false NOT NULL
+);
+
+CREATE TABLE usermusic (
+   id serial primary key,
+   userid character varying(100) references users(userid),
+   musicid integer references musics(id)
+);
+
+CREATE TABLE listmusic (
+   id serial primary key,
+   listid integer references lists(id),
+   musicid integer references musics(id),
+   isref boolean DEFAULT false NOT NULL
+);
+
+CREATE TABLE listowner (
+   id serial primary key,
+   userid character varying(100) references users(userid),
+   listid integer references lists(id)
+);
+
+
+
